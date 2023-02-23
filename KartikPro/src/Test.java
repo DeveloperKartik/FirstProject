23c75427ad72e55407f76b08be879a8dfa2acaf8
kartik17 <126151251+DeveloperKartik@users.noreply.github.com>
2023-02-23 15:54:51 +0530

    Update Test.java
    updates test class

--- KartikPro/src/Test.java
+++ KartikPro/src/Test.java
@@ -4,4 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test class main method");
 	}
+	public void kartikchimethod()
+	{
+	  System.out.println("other method of test class");
+	}
 }
